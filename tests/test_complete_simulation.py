f3b09c47c6ffa8a38bdce0bbb27cfac495a5a4e8
Aaron Bojarski <aaron.bojarski@empa.ch>
2022-03-03 16:04:57 +0100

    added some pytest tests

---
+++ tests/test_complete_simulation.py
@@ -0,0 +1,15 @@
+from dtpy.manager.Manager import Manager
+import os
+from pathlib import Path
+import pytest
+import sys
+
+
+_TEST_CFG_PATH = os.path.dirname(__file__) / Path("./ressources/test_config.yml")
+
+@pytest.mark.skipif(sys.platform != "win32", reason="E+ not running on git ci")
+def test_simulation():
+    example_manager = Manager(_TEST_CFG_PATH)
+
+    example_manager.build_simulation()
+    example_manager.run()
---
+++ tests/test_config.py
@@ -0,0 +1,10 @@
+from dtpy.common.config_loader import load_config
+import os
+from pathlib import Path
+
+
+_TEST_CFG_PATH = os.path.dirname(__file__) / Path("./ressources/test_config.yml")
+
+def test_config_loader():
+    config = load_config(_TEST_CFG_PATH)
+    assert config["RESOLUTION"] == 60
